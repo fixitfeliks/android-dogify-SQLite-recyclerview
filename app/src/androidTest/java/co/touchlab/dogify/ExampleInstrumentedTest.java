8f9cbe96de13c9a2455af1cea601b25afb543976
Feliks P <felixp1337@gmail.com>
2022-01-20 19:02:54 -0500

    Migrated project to androidx Updated target context access in instrumentation test

--- app/src/androidTest/java/co/touchlab/dogify/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/co/touchlab/dogify/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package co.touchlab.dogify;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,7 +22,7 @@ public class ExampleInstrumentedTest
     public void useAppContext() throws Exception
     {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("co.touchlab.dogify", appContext.getPackageName());
     }
